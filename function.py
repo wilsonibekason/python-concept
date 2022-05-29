3a8debef31b3c75fb192b8a07c7299b2d8357b34
wilsonibekason <wanbekgithub@gmail.com>
2022-05-29 12:40:10 -0700

    add function

--- function.py
+++ function.py
@@ -12,6 +12,6 @@ print("finish")
 # keyword Arguement
 # if you have a numerical arguement in your function use the keyword arguement greet_user(firstname='wilson', lastname='ibekason')
 # if you are passing both positional and keyword arguement in a function use positional arguement before using keyword arguement
-
+# EXAMPLE GREET_USER('WILSON', LAST_NAME='IBEKASON')
 
 
