9f670eec24c27a0b36da1f488fe6b192ffd495c3
wilsonibekason <wanbekgithub@gmail.com>
2022-05-29 12:15:24 -0700

    complete emoji converter

--- converter.py
+++ converter.py
@@ -1,5 +1,18 @@
 #challendge
 
+#emoji converter
+message = input('> ')
+# split creates a boundary to seperate a string into multiple words
+words = message.split(" ")
+emoji = {
+    ":)": ".o.",
+    ":(": "-0-",
+}
+output = ""
+for word in words:
+    output += emoji.get(word, word) + " "
+print(output)
+
 phone = input("phone ")
 digits_mapping = {
     "2": "two",
